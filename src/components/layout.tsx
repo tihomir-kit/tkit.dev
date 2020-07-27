5bccf70b7a35bba083608997ca4bbc92fee4c1e9
Tihomir Kit <pootzko@gmail.com>
2020-07-27 09:09:10 +0100

    Added emotion css.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,23 +1,46 @@
 import React from "react";
-
+import { css } from "@emotion/core";
 import { rhythm, scale } from "@/utils";
+
+import { colors } from "../utils/colors";
 import Logo from "./logo";
 import Footer from "./footer";
 
+const appStyle = css`
+  max-width: ${rhythm(34)};
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 220px 1fr;
+    grid-template-rows: auto 1fr auto;
+  }
+`;
+
+const mainStyle = css`
+  flex: 1;
+  padding: 20px;
+`;
+
+const navStyle = css`
+  background-color: ${colors.main};
+  padding: 20px;
+`;
+
+// padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+
 // TODO: use a type instead of any
 const Layout = ({ location, title, children }: any) => {
+  console.log(appStyle);
   return (
-    <div
-      className="app"
-      style={{
-        maxWidth: rhythm(36),
-        // padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}>
-      <nav>
+    <div css={appStyle}>
+      <nav css={navStyle}>
         <Logo />
         Left nav
       </nav>
-      <main>
+      <main css={mainStyle}>
         {children}
         <Footer />
       </main>
