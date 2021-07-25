d95b2327565fcc854efdfe7f19fe1235872116bd
Tihomir Kit <mail@tkit.dev>
2021-07-25 23:17:58 +0100

    SEO title fix.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
-import PersonalBlog from '../containers/home';
+import PersonalBlog from '../containers/home/index';
 import SEO from '../components/seo';
 
 const HomePage = (props: any) => {
@@ -9,7 +9,7 @@ const HomePage = (props: any) => {
 
   return (
     <Layout>
-      <SEO title="Personal Blog Lite" description={data.site.siteMetadata.description} />
+      <SEO title="Tihomir Kit - Dev blog Lite" description={data.site.siteMetadata.description} />
       <PersonalBlog />
     </Layout>
   );
